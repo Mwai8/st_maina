c51c4cf73862ccd9adca675146687208b7f2ebf7
marcmwai <marcmwai@gmail.com>
2021-11-22 14:10:54 +0300

    footer,header,navbar

---
+++ footer.php
@@ -0,0 +1,6 @@
+
+ <script src="assets/js/jquery-3.6.0.min.js"></script>
+    
+    <script src="asset/js/bootstrap.bundle.min.js"></script>   
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--fontawesome-->
-    <script src="https://kit.fontawesome.com/1f8378c25b.js" crossorigin="anonymous"></script>
-    
-    <!--google fonts-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;0,400;1,200;1,400&display=swap" rel="stylesheet">
-
-    <title>Document</title>
-
-    <!--bootstrap css-->
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <!--bootstrap-->
-
-    <!--custom css-->
-    <link rel="stylesheet" href="assets/css/style.css">
-    
-</head>
-<body>
-    
-</body>
-</html>
+<?php include 'navbar.php';?>
+<?php include 'header.php';?>
+<?php include 'footer.php';?>
